390ed9b58ebc9acaec814eb66e78268ebd65b33b
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-06-05 23:27:46 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b){
+    return a+b;
+}
+function subtract(a, b){
+    return a-b;
+}
+function multiply(a, b){
+    return a*b;
+}
+function divide(a, b){
+    return a/b;
+}
+function increment(n=0){
+    n++;
+    return n;
+}
+function decrement(n=0){
+    n--;
+    return n;
+}
+function preserveDecimal(n){
+    n=10.256;
+    return parseFloat(n)
+}
+function makeInt(n,parseInt){
+    parseInt=10;
+    return parseInt;
+}
+const makeInt= (str) => parseInt(str, 10);
+return str*1;
+const preserveDecimal=(Str) =parseFloat(str)
+return NaN;
